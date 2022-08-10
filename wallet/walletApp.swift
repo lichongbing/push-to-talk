b2baa6944f702c9a4e0ecb126e0b1d334576b09a
lichongbing <865143152@qq.com>
2022-08-10 08:56:26 +0800

    Initial Commit

---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by lichongbing on 2022/8/10.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
